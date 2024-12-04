40ea68cc8ed6d3049c1959df079f8fe5a29e758f
Noushin Azami <nooshin.azami@gmail.com>
2024-12-04 17:02:24 -0600

    some slight changes

--- manage_inputs_single.py
+++ manage_inputs_single.py
@@ -6,13 +6,7 @@ import shutil
 # Define the base directory
 base_dir = "single_inputs"
 
-# Step 1: Delete all files that include "log" in their names
-for root, dirs, files in os.walk(base_dir):
-    for file in files:
-        if "log" in file:
-            os.remove(os.path.join(root, file))
-
-# Step 2: Move all files from specified folders into the new 'exaalt' folder
+# Move all files from specified folders into the new 'exaalt' folder
 folders_to_move = [
     "SDRBENCH-exaalt-copper",
     "SDRBENCH-exaalt-helium",
@@ -28,7 +22,7 @@ for folder in folders_to_move:
             for file in files:
                 shutil.move(os.path.join(root, file), os.path.join(exaalt_folder, file))
 
-# Step 3: Delete the three folders
+# Delete the three folders
 for folder in folders_to_move:
     folder_path = os.path.join(base_dir, folder)
     if os.path.exists(folder_path) and os.path.isdir(folder_path):
